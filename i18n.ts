ee2266a0ef2708fde439b2deed11b366581aec25
Aziz <agbalegnonaziz@gmail.com>
2026-06-29 16:19:49 +0000

    fix(i18n): return locale in getRequestConfig

--- i18n.ts
+++ i18n.ts
@@ -4,6 +4,7 @@ export default getRequestConfig(async ({requestLocale}) => {
   let locale = await requestLocale;
   if (!locale) locale = 'fr';
   return {
+    locale,
     messages: (await import(`./messages/${locale}.json`)).default
   };
 });
