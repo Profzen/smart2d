2ea044515c1e2ade0a67984a581103fbaa3a9350
Aziz <agbalegnonaziz@gmail.com>
2026-06-30 18:04:01 +0000

    fix(hero): reduce anchor distance from navbar on homepage (pt-24/pt-32 -> pt-20)

--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -200,7 +200,7 @@ export function HeroSection() {
       </div>
 
       {/* Content */}
-      <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-24 lg:pt-32 pb-16">
+      <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-20 pb-16">
         <div className="mb-4 lg:mb-8">
           <HeroAnchors anchors={homeAnchors} containerClassName="pt-2 pb-4" />
         </div>
